46225254b310ceeb657946543a479eb3881b1ff7
Nick <nikita.glu@icloud.com>
2020-02-06 22:30:32 +0300

    Initial Commit

---
+++ TopMovies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TopMovies
+//
+//  Created by Никита on 06.02.2020.
+//  Copyright © 2020 Nikita Glushchenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
